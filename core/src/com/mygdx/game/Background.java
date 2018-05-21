76ae7d97ce6e22e1bd668505fd27c00e71aa0bfa
Sergey Katashevich <sergeykatashevich@gmail.com>
2018-05-22 02:38:58 +0300

    Adding class Backgroung and picters for graphics

---
+++ core/src/com/mygdx/game/Background.java
@@ -0,0 +1,55 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+public class Background {
+    Texture imgStar;
+    Star[] stars;
+
+    class Star{
+        Vector2 position;
+        float speed;
+
+        public Star(){
+            position = new Vector2(MathUtils.random(0, Gdx.graphics.getWidth()), MathUtils.random(0, Gdx.graphics.getHeight()));
+            speed = MathUtils.random(0.5f, 2.0f);
+        }
+
+        public void update(){
+            position.x -= speed;
+            if (position.x < 0){
+                position.set(Gdx.graphics.getWidth(), MathUtils.random(0, Gdx.graphics.getHeight()));
+                speed = MathUtils.random(0.5f, 2.0f);
+            }
+        }
+    }
+
+    public Background(){
+        imgStar = new Texture("star12.tga");
+        stars = new Star[200];
+        for (int i = 0; i < stars.length; i++){
+            stars[i] = new Star();
+        }
+    }
+
+    public void render(SpriteBatch batch){
+        for (int i = 0; i < stars.length; i++) {
+            float size = stars[i].speed / 2.0f;
+            if (MathUtils.random(0, 50) < 1)
+                size *= 1.8f;
+            batch.draw(imgStar, stars[i].position.x, stars[i].position.y,
+                    6, 6, 12, 12, size, size, 0, 0, 0, 12, 12, false, false);
+        }
+
+    }
+
+    public void update(){
+        for (int i = 0; i < stars.length; i++){
+            stars[i].update();
+        }
+    }
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Background background;
+
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Background();
 	}
 
 	@Override
@@ -22,11 +22,11 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 15/255f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		background.render(batch);
 		batch.end();
 	}
 
 	public void update() {
-
+		background.update();
 	}
 }
